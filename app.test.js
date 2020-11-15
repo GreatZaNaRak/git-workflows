4403afee8c726774fc2598d07d2ee52b0a521fc0
GreatZaNaRak <great2541@windowslive.com>
2020-11-15 23:44:29 +0700

    temping to push a bug code

--- app.test.js
+++ app.test.js
@@ -8,4 +8,8 @@ describe('common logic test', () => {
     test('subtract A by B', () => {
         expect(sub(3, 2)).toBe(1)
     })
+
+    test('should do something wrong', () => {
+        expect(add(1, 2)).toBe(5)
+    });
 })
