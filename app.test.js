bfac17d7af4b518a30eab30d6784a62cfa47bbc8
GreatZaNaRak <great2541@windowslive.com>
2020-11-15 23:32:38 +0700

    Initial Project

---
+++ app.test.js
@@ -0,0 +1,11 @@
+const { add, sub } = require('./app')
+
+describe('common logic test', () => {
+    test('add A and B', () => {
+        expect(add(1, 2)).toBe(3)
+    })
+    
+    test('subtract A by B', () => {
+        expect(sub(3, 2)).toBe(1)
+    })
+})
